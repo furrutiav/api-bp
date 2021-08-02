6cc8a6ba4fb75ed511d8c262dc1cf03b4773e005
furrutiav <74210202+furrutiav@users.noreply.github.com>
2021-08-02 18:17:33 -0400

    update gitignore: models.py

--- API/flask_api.py
+++ API/flask_api.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
+from flask_cors import CORS
 import base64
+
 from time import sleep
 from API.models import solve
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
 api = Api(app)
 
 
